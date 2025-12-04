08de5ff2bc1d7dc9d5a1b2b8fa8c13ac80a0a879
Matt Maul <matt@portofmaul.com>
2025-12-04 15:29:01 -0500

    Day 4.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,5 @@
 ((nil . ((indent-tabs-mode . nil)
          (tab-width . 4)
          (default-tab-width . 4)
-         (python-indent . 4))))
+         (python-indent . 4)
+         (python-indent-offset . 4))))
---
+++ 04/part_1.py
@@ -0,0 +1,32 @@
+import sys
+
+def neighbors(rolls, row, col):
+    neighbor_list = []
+    for r in [row, row - 1, row + 1]:
+        for c in [col, col - 1, col + 1]:
+            if r != row or c != col:
+                neighbor_list.append(rolls[r][c])
+    return neighbor_list
+
+if __name__ == "__main__":
+    rolls = []
+    with open(sys.argv[1]) as file_handle:
+        for line in file_handle:
+            row = [' ']
+            for char in line.strip():
+                row.append(char)
+            row.append(' ')
+            rolls.append(row)
+    width = len(rolls[0])
+    rolls.insert(0, [' '] * width)
+    rolls.append([' '] * width)
+
+    count = 0
+    for row_num, row in enumerate(rolls):
+        for col_num, cell in enumerate(row):
+            if cell == '@':
+                neighbor_rolls = len([neighbor for neighbor in neighbors(rolls, row_num, col_num) if neighbor == '@'])
+                if neighbor_rolls < 4:
+                    print(f"{row_num}, {col_num}")
+                    count += 1
+    print(count)
